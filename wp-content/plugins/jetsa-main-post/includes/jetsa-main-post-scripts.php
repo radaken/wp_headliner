a031499d8cc549669b7c8d3789fe342304a84556
Alex <alexon249@gmail.com>
2023-01-25 02:23:40 +0300

    Using wp_query

--- wp-content/plugins/jetsa-main-post/includes/jetsa-main-post-scripts.php
+++
@@ -1,9 +0,0 @@
-<?php
-  // Add Scripts
-  function jmp_add_scripts(){
-    // Add Main CSS
-    wp_enqueue_style('jmp-style', plugins_url(). '/jetsa-main-post/css/style.css');
-
-  }
-
- add_action('wp_enqueue_scripts', 'jmp_add_scripts');
